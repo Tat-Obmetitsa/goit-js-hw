3c3e19a2f9dd670152089c5db929d6635a69a35b
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2020-11-14 23:21:16 +0200

    m5

---
+++ js/module-5/task-2.js
@@ -0,0 +1,22 @@
+class User {
+  constructor(name, age, followers) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers
+  }
+  getInfo = function () {
+    return `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+  }
+}
+console.log(typeof User);
+// 'function'
+
+const mango = new User('Mango', 2, 20);
+console.log(mango.getInfo());
+// 'User Mango is 2 years old and has 20 followers'
+
+console.log(typeof mango.getInfo);
+// 'function'  
+
+const poly = new User('Poly', 3, 17);
+console.log(poly.getInfo());
